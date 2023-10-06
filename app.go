dd424065c024b3b7d2c6f45064b98ec9da671343
Pyunghyuk Yoo <pyunghyuk.yoo@payhere.in>
2023-10-06 19:27:54 +0900

    feat: get random image

--- app.go
+++ app.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sort"
 )
 
 // App struct
 type App struct {
 	ctx context.Context
+}
+
+type RandomImage struct {
+	Message string
+	Status  string
+}
+
+type AllBreeds struct {
+	Message map[string]map[string][]string
+	Status  string
+}
+
+type ImagesByBreed struct {
+	Message []string
+	Status  string
 }
 
 // NewApp creates a new App application struct
@@ -38,7 +58,63 @@ func (a *App) shutdown(ctx context.Context) {
 	// Perform your teardown here
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func (a *App) GetRandomImageUrl() string {
+	response, err := http.Get("https://dog.ceo/api/breeds/image/random")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data RandomImage
+	json.Unmarshal(responseData, &data)
+
+	return data.Message
+}
+
+func (a *App) GetBreedList() []string {
+	var breeds []string
+
+	response, err := http.Get("https://dog.ceo/api/breeds/list/all")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data AllBreeds
+	json.Unmarshal(responseData, &data)
+
+	for k := range data.Message {
+		breeds = append(breeds, k)
+	}
+
+	sort.Strings(breeds)
+
+	return breeds
+}
+
+func (a *App) GetImageUrlsByBreed(breed string) []string {
+
+	url := fmt.Sprintf("%s%s%s%s", "https://dog.ceo/api/", "breed/", breed, "/images")
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data ImagesByBreed
+	json.Unmarshal(responseData, &data)
+
+	return data.Message
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,25 @@
-import { Button } from "@/components/ui/button"
-import React from "react"
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+import { GetRandomImageUrl } from "../wailsjs/go/main/App.js";
 
 function App() {
-  const [count, setCount] = React.useState(0)
+  const [randomImageUrl, setRandomImageUrl] = useState<string>("");
+  const fetchRandomImage = async () => {
+    const image = await GetRandomImageUrl();
+    setRandomImageUrl(image);
+  };
 
   return (
     <div className="min-h-screen bg-white grid place-items-center mx-auto py-8">
       <div className="text-blue-900 text-2xl font-bold flex flex-col items-center space-y-4">
         <h1>Vite + React + TS + Tailwind + shadcn/ui</h1>
-        <Button onClick={() => setCount(count + 1)}>Count up ({count})</Button>
+        <Button onClick={() => fetchRandomImage()}>fetch RandomImage</Button>
+        {randomImageUrl !== "" && (
+          <img src={randomImageUrl} className="w-[320px]" alt="" />
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
